5b7f2a737fceaf7b7eb036a128538bdba01220f3
vihernandes <vinicius.hernandes@indt.org.br>
2022-04-01 21:03:57 -0300

    base client-relationship service created

---
+++ client-relationship/src/main/java/com/clientRelationship/project/models/exceptions/Unauthorized.java
@@ -0,0 +1,7 @@
+package com.clientRelationship.project.models.exceptions;
+
+public class Unauthorized extends Exception { 
+    public Unauthorized(String message){
+        super(message);
+    }
+}
